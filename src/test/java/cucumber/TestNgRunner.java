dad26fb85ebbf1d7003c3a5d38ae02f9e50303b1
Archana Regu <archana.regu@gmail.com>
2024-08-23 15:12:41 -0500

    third

--- src/test/java/cucumber/TestNgRunner.java
+++ src/test/java/cucumber/TestNgRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/Featurefile",glue="StepDefination",monochrome=true,
+@CucumberOptions(features="src/test/resources/Featurefile",glue="StepDefination",monochrome=true,
 tags= "@SmokeTest or @Monkey",plugin= {"html:target/cucumber.html","json:target/cucumber.json",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class TestNgRunner extends AbstractTestNGCucumberTests {
