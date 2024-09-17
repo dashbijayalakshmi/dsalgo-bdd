4892c2869457c5b749d38467a0d02747bded648f
Neha Patni <npatni25@gmail.com>
2024-09-16 22:36:44 -0400

    Adding Login Page Test Cases total 8

--- src/test/java/Pageobjects/Signinpageobjects.java
+++ src/test/java/Pageobjects/Signinpageobjects.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -17,6 +18,9 @@ public class Signinpageobjects {
 	private By password=By.cssSelector("#id_password");
 	private By login_btn=By.xpath("//input[@value='Login']");
 	private By alert_message=By.xpath("//div[@role='alert']");
+	private By error_invalid_userPass=By.xpath("//div[contains(text(), 'Invalid Username and Password')]");
+	private By registerbtn_on_loginpage=By.xpath("//div[@class='col-sm']/a[@href='/register']");
+	
 	WebDriver driver;
 	public Signinpageobjects(WebDriver driver) {
 		this.driver=driver;
@@ -58,5 +62,32 @@ public class Signinpageobjects {
 		WebElement you_login=driver.findElement(alert_message);
 		return you_login;
 	}
-
+	
+	public void press_tab_on_username() {
+		driver.findElement(username).sendKeys(Keys.TAB);
+		
+	}
+	public void press_tab_on_password() {
+		driver.findElement(password).sendKeys(Keys.TAB);
+		
+	}
+	public void error_username_req()
+	{
+		String message=driver.findElement(username).getAttribute("validationMessage");
+		System.out.println("Error - "+message+ " displayed when username is blank");
+	}
+	public void error_password_req()
+	{
+		String message=driver.findElement(password).getAttribute("validationMessage");
+		System.out.println("Error - "+message+ " displayed when password is blank");
+	}
+	public void error_invalid_userandpass()
+	{
+		driver.findElement(error_invalid_userPass).getText();
+		
+	}
+	public void click_register_from_signingpage()
+	{
+		driver.findElement(registerbtn_on_loginpage).click();
+	}
 }
